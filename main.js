7fe8fc4703f0ce6ee8cbda177ab5621a8e59dbd0
Conor <conorjm89@gmail.com>
2020-12-10 00:12:17 +0000

    Upgraded static html to DOM

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
-let myLibrary = [];
-
-
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.info =  `${title} by ${author}, ${pages} pages, ${read}`;
+    this.info =  `${title} by ${author}, ${pages} pages, ${read} pages read`;
 }
 
+const firstBook = new Book("Starship Troopers", "Robert A. Heinlein", 263, 263);
+const secondBook = new Book("Hyperion", "Dan Simmons", 482, 482);
+
+let myLibrary = [firstBook, secondBook];
+
+// HTML for below removed, update to DOM
 function addBookToLibrary() {
     let title = document.getElementById("title").value;
     let author = document.getElementById("author").value;
@@ -29,18 +32,94 @@ function putBookOnShelf() {
     return list;
 }
 
-const addButton = document.querySelector("button.add-book");
-addButton.onclick = function () {
-    addBookToLibrary()
-    document.getElementById("shelf").innerHTML = "";
-    document.getElementById("shelf").appendChild(putBookOnShelf());
+function clearInputs() {
+    inputs = Array.from(document.querySelectorAll("input"));
+    for (let i = 0; i < inputs.length; i ++) {
+        inputs[i].value = "";
+    }
+}
+
+document.getElementById("shelf").appendChild(putBookOnShelf());
+
+const newButton = document.createElement("button");
+newButton.textContent = "New Book";
+document.getElementById("add-form").appendChild(newButton);
+
+const addButton = document.createElement("button");
+addButton.textContent = "Add Book";
+
+
+
+newButton.onclick = function () {
+    // Get line breaks working for after each  input box
+    const formContainer = document.createElement("div");
+    formContainer.setAttribute("id", "form");
+    document.getElementById("add-form").appendChild(formContainer);
+    // display inputs
+    const titleInput = document.createElement("input");
+    titleInput.type = "text";
+    titleInput.id = "title";
+
+    const titleLabel = document.createElement("label");
+    titleLabel.htmlFor = "title";
+    titleLabel.textContent = "Title";
+
+    titleLabel.appendChild(titleInput);
+    formContainer.appendChild(titleLabel);
+
+    const authorInput = document.createElement("input");
+    authorInput.type = "text";
+    authorInput.id = "author";
+
+    const authorLabel = document.createElement("label");
+    authorLabel.htmlFor = "author";
+    authorLabel.textContent = "Author";
+
+    authorLabel.appendChild(authorInput);
+    formContainer.appendChild(authorLabel);
+
+    const pagesInput = document.createElement("input");
+    pagesInput.type = "text";
+    pagesInput.id = "pages";
+
+    const pagesLabel = document.createElement("label");
+    pagesLabel.htmlFor = "pages";
+    pagesLabel.textContent = "Pages";
+
+    pagesLabel.appendChild(pagesInput);
+    formContainer.appendChild(pagesLabel);
+
+    const readInput = document.createElement("input");
+    pagesInput.type = "text";
+    pagesInput.id = "read";
+
+    const readLabel = document.createElement("label");
+    readLabel.htmlFor = "read";
+    readLabel.textContent = "Pages read";
+
+    readLabel.appendChild(readInput);
+    formContainer.appendChild(readLabel);
+
+    // display Add Book Button
+    
+    formContainer.appendChild(addButton);
 }
 
 
+addButton.onclick = function () {
+    addBookToLibrary()
+    document.getElementById("shelf").textContent = "";
+    document.getElementById("shelf").appendChild(putBookOnShelf());
+    clearInputs();
+}
+
+// Only display books
+// Button that says NEW BOOK
+// Click NEW BOOK
+// Inputs and Add Book appear
+// Tidy up New Book on click function
+// Hook up addButton functions to new DOM elements
 
 
 
-// make add book button
-//  make onclick event for add book button
-// addBookToLibrary runs on click
-// display library above button
+
